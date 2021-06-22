996515a31c1006fa1ad84ed2e04bbec1a884699c
MakerHu <1003964217@qq.com>
2021-06-22 19:52:35 +0800

    胡江浩 上传Oauth2

---
+++ bookshop_auth/src/main/java/com/javaee/bookshop_auth/BookshopAuthApplication.java
@@ -0,0 +1,13 @@
+package com.javaee.bookshop_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopAuthApplication.class, args);
+    }
+
+}
---
+++ bookshop_auth/src/test/java/com/javaee/bookshop_auth/BookshopAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaee.bookshop_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
